09cfebfdaf72db8660ad67d61b603192855b25e5
RyanChehab <ryanchehab20@gmail.com>
2024-11-18 23:42:38 +0200

    php <=> inserted signup form in database

--- src/backend/signup.php
+++ src/backend/signup.php
@@ -7,3 +7,25 @@ if ($_SERVER['REQUEST_METHOD']==='POST'){
     $password = $_POST['password'];
     $user_type = $_POST['user_type'];
 }
+
+$hashed = password_hash($password,PASSWORD_DEFAULT);
+
+$banned = 0;
+
+$sql = "INSERT INTO users(name,email,password,user_type,banned) VALUES(?, ?, ?, ?, ?)";
+
+$stmt = $conn->prepare($sql);
+
+$stmt->bind_param("ssssi",$name,$email,$hashed,$user_type,$banned);
+
+if ($stmt->execute()){
+    $response =[];
+    $response['status'] = "success";
+    http_response_code(200);
+    echo json_encode($response);
+}else{
+    $response =[];
+    $response['status'] = "failed";
+    http_response_code(400);
+    echo json_encode($response);
+}
