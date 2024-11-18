201c86250dddde950cf930371f3046231483e319
RyanChehab <ryanchehab20@gmail.com>
2024-11-18 23:57:05 +0200

    php <=> responded with login status

--- src/backend/login.php
+++ src/backend/login.php
@@ -8,4 +8,31 @@ if ($_SERVER['REQUEST_METHOD']==='POST'){
 
 $sql = 'SELECT password FROM users where email = ?';
 
+$stmt = $conn->prepare($sql);
 
+$stmt->bind_param("s",$email);
+
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+if($result ->num_rows>0){
+    $row = $result->fetch_assoc();
+    
+    $hashed = $row['password'];
+
+    if(password_verify($password,$hashed)){
+        $response = []; 
+        $response['status']= "success";
+        $response['message'] = 'login successfull';
+        http_response_code(200);
+        echo json_encode($response);
+    }else{
+        $response = []; 
+        $response['status']= "failed";
+        $response['message'] = 'login failed';
+        http_response_code(400);
+        echo json_encode($response);
+    }
+
+}
