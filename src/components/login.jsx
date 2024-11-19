39196913f7957e527a3b9a7aa3f5320aef2a0069
RyanChehab <ryanchehab20@gmail.com>
2024-11-20 01:59:59 +0200

    jsx <=> decoded token

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,9 +1,10 @@
 import React,{useState} from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 const Login = () =>{
-
+    // init variables along with there setting functions
     const [message,setMessage] = useState("")
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
@@ -16,8 +17,16 @@ const Login = () =>{
                 "email" : email,
                 "password" : password,
             })
+            // when login successful
             setMessage(response.data.message);
             console.log(response.data)
+            
+            // decoding the passed token's payload
+            const decodedToken = jwtDecode(data.token);
+            // storing the token in localStorage
+            localStorage.setItem("token",data.token)
+
+
         }catch(error){
             if (error.response){
                 setMessage(error.response.data.message);
