ac278aa8c8a87e93bb7dacd8ef8e649a04ec6702
RyanChehab <ryanchehab20@gmail.com>
2024-11-19 23:07:16 +0200

    js <=> imported router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Signup from './components/Signup';
+import Login from './components/Login';
 
 function App() {
   return (
+    <Router>
+      <Routes>
+        <Route path='/Signup' element = {<Signup />} />
+        <Route path='/Login' element = {<Login />} />
+      </Routes>
+    </Router>
     
   );
 }
