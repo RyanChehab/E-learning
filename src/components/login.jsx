c2fd4cf67658d850b822dee490397f530d401fed
RyanChehab <ryanchehab20@gmail.com>
2024-11-19 21:58:56 +0200

    jsx <=> added to the handle submission functionin

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -2,16 +2,23 @@ import React,{useState} from "react";
 
 const login = () =>{
 
+    const [message,setMessage] = useState("")
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         try{
-            
+            const response = await axios.post("http://localhost/elearning/src/backend/login.php",{
+                "email" : email,
+                "password" : password,
+            })
+            setMessage(response.data.message);
         }catch(error){
-
+            if (error.response){
+                setMessage(response.data.message);
+            }
         }
     }
 
