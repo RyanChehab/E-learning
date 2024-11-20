33fdbe50948132ae78ebad7371601841bad270f8
RyanChehab <ryanchehab20@gmail.com>
2024-11-20 08:33:00 +0200

    jsx <=> added buttons for categories in admin panel

---
+++ src/components/AdminPanel.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+
+const Admin = () =>{
+    return(
+        <>
+        <button className="admin-btn">Students</button>
+        <button className="admin-btn">Instructos</button>
+        <button className="admin-btn">Courses</button>
+        </>
+
+    )
+}
+
+export default Admin;
