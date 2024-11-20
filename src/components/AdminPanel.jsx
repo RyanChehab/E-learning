43d33f415db9d8fc46d06606c32fcb5ac5b1071a
RyanChehab <ryanchehab20@gmail.com>
2024-11-20 09:03:03 +0200

    jsx <=> rendered active section

--- src/components/AdminPanel.jsx
+++ src/components/AdminPanel.jsx
@@ -3,12 +3,19 @@ import React, {useState} from "react";
 const Admin = () =>{
     const [activeSection, setActiveSection] = useState("students")
     return(
+<>
         <div className="AdminPanel">
         <button className="admin-btn" onClick={setActiveSection("students")}>Students</button>
         <button className="admin-btn" onClick={setActiveSection("instructors")}>Instructos</button>
         <button className="admin-btn"  onClick={() => setActiveSection("courses")}>Courses</button>
         </div>
 
+        <div className="admin-content">
+        {activeSection === "students" && <StudentsList />}
+        {activeSection === "instructors" && <InstructorsList />}
+        {activeSection === "courses" && <CoursesList />}
+        </div>
+</>
     )
 }
 
