af1d1be5ea186230037aff8117a18500814b68e8
RyanChehab <ryanchehab20@gmail.com>
2024-11-20 10:21:04 +0200

    php <=> decoded json data fetched from the user

--- src/backend/read.php
+++ src/backend/read.php
@@ -1,6 +1,13 @@
 <?php
 include 'connection';
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $input = json_decode(file_get_contents("php://input"),true);
+    $user_type= $input['user_type'];
+}
+
 $sql = "SELECT * FROM users where user_type = ?";
 
 $stmt = $conn->prepare($sql);
+
+$stmt->bind_param("s","student")
