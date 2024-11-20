b18422d337c99b75473e0aaab22c1d5c4b7d5ab6
RyanChehab <ryanchehab20@gmail.com>
2024-11-20 10:40:26 +0200

    php<=> debugged read api

--- src/backend/read.php
+++ src/backend/read.php
@@ -1,12 +1,12 @@
 <?php
-include 'connection';
+include 'connection.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
     $input = json_decode(file_get_contents("php://input"),true);
     $user_type= $input['user_type'];
 }
 
-$sql = "SELECT * FROM users where user_type = ?";
+$sql = "SELECT user_id, email, banned FROM users where user_type = ?";
 
 $stmt = $conn->prepare($sql);
 
@@ -17,16 +17,18 @@ $stmt->execute();
 $result = $stmt->get_result();
 
 if ($result->num_rows > 0){
-    $row = [];
+    $rows = [];
     while ($row = $result->fetch_assoc()){
-        $row[] = $row;
+        $rows[] = $row;
     }
     $response = [];
     $response['status'] = 'success';
-    $respone['result']= $row;
+    $response['result']= $rows;
     http_response_code(200);
-    json_encode($response);
+    echo json_encode($response);
 }else{
-    $respone['status'] = 'success';
+    $respone['status'] = 'no users found';
     $response['result'] = [];
+    http_response_code(200);
+    echo json_encode($response);
 }
