72fa4dd8a990613a883865de8e493e1ff6fcbaa0
RyanChehab <ryanchehab20@gmail.com>
2024-11-21 06:35:33 +0200

    jsx <=> added useEffect having students as its dependancy array

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,28 +1,49 @@
 import React,{useState,useEffect} from "react";
 import axios from "axios";
 
-const Students = ()=>{
+const StudentsList = ()=>{
 
     const [students,setStudents] = useState([]);
 
-    const read= async() =>{
+    const read = async() =>{
         try{
             const response = await axios.post("http://localhost/elearning/src/backend/read.php",{
                 user_type: 'student',
             })
             console.log(response.data)
-        }catch(erro){
-            console.error(response.error)
+            setStudents(response.data.result)
+            // console.log(students)
+        }catch(error){
+            
+            console.error("failed fetch")
         }
     }
 
+    useEffect(() => {
+        read(); // Fetch students on component mount
+      }, []);
+
     useEffect(()=>{
-        read();
-    },[])
-    
+       console.log("updated students:",students)
+    },[students])
+
+    const banStudent =(studentId) => {
+        console.log(`ban student with ID: ${studentId}`)
+    }
+
     return(
-        <div></div>
+        <div>
+            <h2>Students Lists</h2>
+            <ul>
+        {students.map((student, index) => (
+          <li key={index}>
+            {student.id} - {student.email}{" "}
+            <button onClick={() => banStudent(student.id)}>Ban</button>
+          </li>
+        ))}
+      </ul>
+        </div>
     )
 }
 
-export default Student;
+export default StudentsList;
