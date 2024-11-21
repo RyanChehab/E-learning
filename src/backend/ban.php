5842771c8d310ce0fd8b09f2ec605545507275f4
RyanChehab <ryanchehab20@gmail.com>
2024-11-21 08:39:11 +0200

    pghp <=> debugged the sql

--- src/backend/ban.php
+++ src/backend/ban.php
@@ -7,7 +7,7 @@ if ($_SERVER['REQUEST_METHOD']==='POST'){
     $user_id = $input['user_id'];
 }
 
-$sql = 'UPDATE users SET banned = 1 WHERE id = ?';
+$sql = 'UPDATE users SET banned = 1 WHERE user_id = ?';
 
 $stmt= $conn->prepare($sql);
 
