92338ae00c2ec2d3e6d9ef2625e8557aaa460653
RyanChehab <ryanchehab20@gmail.com>
2024-11-21 09:48:42 +0200

    jsx <=> created courses component

---
+++ src/components/Courses.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+
+const CourseList = ()=>{
+    read = async ()=>{
+        try{
+
+        }catch(error){
+            error.log("failed to read")
+        }
+    }
+}
