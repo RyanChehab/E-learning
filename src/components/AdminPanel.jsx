c2a0f7c6f5b2cb6dda2add8c68a9aa485044656d
RyanChehab <ryanchehab20@gmail.com>
2024-11-21 09:01:43 +0200

    jsx <=> simported InstructorsList

--- src/components/AdminPanel.jsx
+++ src/components/AdminPanel.jsx
@@ -1,21 +1,26 @@
 import React, {useState} from "react";
-
+import StudentsList from "./Student";
+import InstructorsList from "./Instructors";
 const Admin = () =>{
     const [activeSection, setActiveSection] = useState("students")
     return(
 <>
+{/* setActiveSection("students") */}
         <div className="AdminPanel">
-        <button className="admin-btn" onClick={setActiveSection("students")}>Students</button>
-        <button className="admin-btn" onClick={setActiveSection("instructors")}>Instructos</button>
+        <button className="admin-btn" onClick={()=>{
+        setActiveSection((show)=>(show === "students"? "" : "students"))
+        }}>Students</button>
+        <button className="admin-btn" onClick={()=>setActiveSection("instructors")}>Instructos</button>
         <button className="admin-btn"  onClick={() => setActiveSection("courses")}>Courses</button>
         </div>
 
         <div className="admin-content">
         {activeSection === "students" && <StudentsList />}
-        {activeSection === "instructors" && <InstructorsList />}
-        {activeSection === "courses" && <CoursesList />}
-        </div>
-</>
+        {activeSection === "Instructors" && <InstructorsList/>}
+        {/* {activeSection === "instructors" && <Instructors/>}
+        {activeSection === "courses" && <Courses />} */}
+        </div> 
+        </>
     )
 }
 
