1daf571de0890024cfb5f0265d39ebfd314a4da6
RyanChehab <ryanchehab20@gmail.com>
2024-11-21 13:15:20 +0200

    jsx <=> set the retrieved courses in state

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,11 +1,32 @@
-import React, {useState} from "react";
+import React,{useState,useEffect} from "react";
+import axios from "axios";
 
 const CourseList = ()=>{
-    read = async ()=>{
-        try{
+    const [courses,setCourses] = useState([]);
 
+    const read = async ()=>{
+        try{
+            const response = await axios.post("http://localhost/elearning/src/backend/read_courses.php",{
+                header:{
+                    "Content-Type": "application/json",
+                } 
+            })
+            
+            setCourses(response.data.result)
         }catch(error){
             error.log("failed to read")
         }
     }
+
+    useEffect(()=>{
+        read();
+    },[])
+
+    useEffect(()=>{
+        console.log("updated courses" , courses)
+    },[courses])
 }
+
+
+
+export default CourseList;
