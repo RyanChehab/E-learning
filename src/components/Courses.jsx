1d4a7f40655b71cfc53bf5afcbd19931af8ef905
RyanChehab <ryanchehab20@gmail.com>
2024-11-21 16:31:01 +0200

    jsx <=> returned course list

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -17,16 +17,31 @@ const CourseList = ()=>{
             error.log("failed to read")
         }
     }
-
+    // calling the api
     useEffect(()=>{
         read();
     },[])
-
+    // updating the course list
     useEffect(()=>{
         console.log("updated courses" , courses)
     },[courses])
+
+    const addCourse = ()=>{
+        console.log("working")
+    }
+
+return(
+    <div>
+        <h2>Course List</h2>
+        <ul>
+            {courses.map((course, index) => (
+            <li key={index}>
+                {course.title} - {course.description}{" "}
+                <button onClick={() => addCourse()}>Add course</button>
+            </li>
+            ))}
+        </ul>
+    </div>
+)
 }
-
-
-
 export default CourseList;
