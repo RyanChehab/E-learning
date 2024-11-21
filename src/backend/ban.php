c96a424351f26f35c89156ee5893ab471934cab1
RyanChehab <ryanchehab20@gmail.com>
2024-11-21 07:12:07 +0200

    php <=> updated the banned field

--- src/backend/ban.php
+++ src/backend/ban.php
@@ -1,3 +1,17 @@
 <?php
 
 include 'connection.php';
+
+if ($_SERVER['REQUEST_METHOD']==='POST'){
+    $input = json_decode(file_get_contents("php://input"),true);
+    $user_id = $input['user_id'];
+}
+
+$sql = 'UPDATE users SET banned = 1 WHERE id = ?';
+
+$stmt= $conn->prepare($sql);
+
+$stmt-> bind_param("i", $user_id);
+
+$stmt->execute();
+
