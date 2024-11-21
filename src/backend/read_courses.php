4de3c1045e8d82ba9f5620132ddb2e4d52ce88f6
RyanChehab <ryanchehab20@gmail.com>
2024-11-21 10:00:31 +0200

    php <=> executed sql query

--- src/backend/read_courses.php
+++ src/backend/read_courses.php
@@ -3,5 +3,16 @@
 include 'connection.php';
 
 if ($_SERVER['REQUEST_METHOD']==='POST'){
-    $input = json_decode(file_get_contents("php://input"),true)
+    $sql = "SELECT * from courses";
+
+    $stmt = $conn->prepare($sql);
+
+    $stmt->execute();
+
+    $stmt->get_result();
+    
+    $rows=[];
 }
+
+// $input = json_decode(file_get_contents("php://input"),true)
+//     $title = $input['title'];
